7aae23d8517d4083376594469bd44027efc5b5c9
Rosemberg de Lima <rosembergdlt@gmail.com>
2016-10-30 13:29:04 -0300

    Pequenos nome alterados

---
+++ app/src/main/java/com/example/rosemberg/sine/activities/ListarSinesCampina.java
@@ -0,0 +1,15 @@
+package com.example.rosemberg.sine.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.rosemberg.sine.R;
+
+public class ListarSinesCampina extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_sines_campina);
+    }
+}
